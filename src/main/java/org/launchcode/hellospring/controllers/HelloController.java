88ecd6faa3e18318b58cf5761f9eed680a0dffce
molalenim <molaleni.mabaso@gmail.com>
2024-05-05 19:29:41 -0400

    Part5:Path Param

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,4 +29,12 @@ public class HelloController {
     public String helloWithQueryParam(@RequestParam String name) {
         return "hello, " + name + "!!!";
     }
+
+    // handles request of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "hello, " + name + "!!!";
+    }
+
 }
