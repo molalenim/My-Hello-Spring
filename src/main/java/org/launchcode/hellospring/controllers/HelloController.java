f3c2188ce5965e8c0ef4d4f9dd7a0bd5c560e0cb
molalenim <molaleni.mabaso@gmail.com>
2024-05-05 18:53:33 -0400

    Part4:Query Param

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,22 +2,30 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    //Handles request at path /hello
+/*    //Handles request at path /hello
     @GetMapping("hello")
     @ResponseBody
     public String hello() {
         return "Hello, Spring!";
-    }
+    }*/
 
     // lives /hello/goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // handles request of the form /hello?name=<name>
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "hello, " + name + "!!!";
     }
 }
