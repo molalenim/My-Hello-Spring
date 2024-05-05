32cf0380bad291949738adda42c6dec436b168a8
molalenim <molaleni.mabaso@gmail.com>
2024-05-05 16:47:04 -0400

    Part one video

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+        return "Hello, Spring!";
+
+        //this is part one
+    }
+}
