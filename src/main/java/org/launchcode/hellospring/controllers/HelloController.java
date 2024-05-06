b7e6b9cb54caa3f2eb1449bc48928d238e4089fb
molalenim <molaleni.mabaso@gmail.com>
2024-05-05 20:33:29 -0400

    Part7: Class Annotations

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
 /*    //Handles request at path /hello
@@ -15,35 +17,39 @@ public class HelloController {
 
     // lives /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
+
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
+    //lives /hello/hello
     // handles request of the form /hello?name=<name>
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "hello, " + name + "!!!";
     }
 
     // handles request of the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
+
     public String helloWithPathParam(@PathVariable String name) {
         return "hello, " + name + "!!!";
     }
 
+    // handles POST request from form submission
+    @PostMapping("hello")
+    public String helloFormSubmit(@RequestParam String name) {
+        return "hello, " + name + "!!!";
+    }
 
     // lives /hello/form
     // displays a form with text input
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='hello' method='post'>" + // submit a request to /hello
+                "<form action='/hello/hello' method='post'>" + // submit a request to /hello
                 "<input type='text' name='name'>" +
                 "<input type='submit' value='Greet me!!'>" +
                 "</form>" +
